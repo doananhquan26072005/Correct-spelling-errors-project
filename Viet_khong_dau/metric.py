0f8a8dd95b5fe2460df28ef0bb96992d4ff01ea6
surmullet <lviethoang22@gmail.com>
2026-05-29 14:24:17 +0700

    update metric

--- Viet_khong_dau/metric.py
+++ Viet_khong_dau/metric.py
@@ -1,25 +1,5 @@
+import Levenshtein
 from typing import List
-
-
-def levenshtein_distance(a: str, b: str) -> int:
-    n, m = len(a), len(b)
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
-
-    for i in range(n + 1):
-        dp[i][0] = i
-    for j in range(m + 1):
-        dp[0][j] = j
-
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            cost = 0 if a[i - 1] == b[j - 1] else 1
-            dp[i][j] = min(
-                dp[i - 1][j] + 1,
-                dp[i][j - 1] + 1,
-                dp[i - 1][j - 1] + cost,
-            )
-
-    return dp[n][m]
 
 
 def word_accuracy(preds: List[str], targets: List[str]):
@@ -73,7 +53,7 @@ def compute_text_metrics(inputs: List[str], preds: List[str], targets: List[str]
                 correct_chars += 1
 
         total_chars += max(len(pred), len(target))
-        total_edit_distance += levenshtein_distance(pred, target)
+        total_edit_distance += Levenshtein.distance(pred, target)
         total_target_chars += max(1, len(target))
 
     return {
